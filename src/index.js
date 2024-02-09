90f5ce62c782d782a4243f34b03de05a331b7aed
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-02-09 19:39:28 +0100

    added more weather data+

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
-}
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  temperature = response.data.temperature.current;
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes},`;
+}
 function searchCity(city) {
   let apiKey = "a41b6cct8f269ddeo4da44bf9300d179";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
