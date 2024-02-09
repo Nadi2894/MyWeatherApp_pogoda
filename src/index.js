abcc84cf4f116565ddbb2f1e58ddfc3c67c28cac
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-02-09 18:56:03 +0100

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "a41b6cct8f269ddeo4da44bf9300d179";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function form(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search_input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search_form");
 searchForm.addEventListener("submit", form);
+searchCity("London");
